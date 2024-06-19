1d3ad526da0baa5bffb5776b986ce52f5e1ca0ef
httpsbina <binafshab@nycstudents.net>
2024-06-19 15:41:20 -0400

    Update Problem02.py

--- Python Tutorial /Problem02.py
+++ Python Tutorial /Problem02.py
@@ -1,1 +1,8 @@
 # Write a function to find the factors of a positive number. Test the function on several examples.
+
+def b(x):
+  factors = [] # ARRAYS, to hold the all the postive numbers !!!
+  for i in range(1, n+1): # Looping through all postive numbers 
+    if n % i == 0: # If there is no reminder then...
+      factors.append(i) # Add to the array
+    return factors; # Return the array !!!
