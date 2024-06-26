590857de13a85769f44943fd53c4aa34aa531085
httpsbina <binafshab@nycstudents.net>
2024-06-26 13:33:43 -0400

    Create Problem04.py

---
+++ Python Tutorial /Problem04.py
@@ -0,0 +1,10 @@
+# Question : Implement a class that has at least two methods:
+# getString: to get a string from console input (Hint: use input())
+# printString: to print the string in upper case (Hint: use upper())
+# Instantiate your class and test its methods.
+
+class a:
+    def getString(self):
+        self.string = input("Enter a string: ")
+    def printString(self):
+        print(self.string.upper())
