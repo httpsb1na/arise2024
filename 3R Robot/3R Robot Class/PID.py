69cb5f1e71f32d05c37912bc0cec50e488f0ee69
httpsbina <binafshab@nycstudents.net>
2024-07-30 10:21:46 -0400

    Update and rename DONTTOUCH.py to PID.py

--- 3R Robot/3R Robot Class/PID.py
+++ 3R Robot/3R Robot Class/PID.py
@@ -32,11 +32,11 @@ def load_robot(urdf_path, basePosition, baseOrientation=[0, 0, 0, 1]):
 
 def create_ball():
     """Create a ball with collision and visual properties and a constraint to hold it in place."""
-    ball_radius = 0.1
+    ball_radius = 0.125
     ball_mass = 1.6
     ball_col_id = p.createCollisionShape(p.GEOM_SPHERE, radius=ball_radius)
     ball_vis_id = p.createVisualShape(p.GEOM_SPHERE, radius=ball_radius, rgbaColor=[1, 0, 0, 1])
-    ball_id = p.createMultiBody(ball_mass, ball_col_id, ball_vis_id, basePosition=[0.1, 0, 0])
+    ball_id = p.createMultiBody(ball_mass, ball_col_id, ball_vis_id, basePosition=[0.1, 0, 0.0])
     constraint_id = p.createConstraint(ball_id, -1, -1, -1, p.JOINT_FIXED, [0, 0, 0], [0, 0, 0], [0.1, 0, 0])
     return ball_id, constraint_id
 
@@ -60,6 +60,17 @@ def simulate_kick(robot_id, robot_id2, ball_id, constraint_id):
                 p.setJointMotorControl2(robot_id, 2, p.POSITION_CONTROL, targetPosition=0, force=4500)
                 p.setJointMotorControl2(robot_id, 3, p.POSITION_CONTROL, targetPosition=0, force=4500)
 
+        if 145 <= i < 165:
+            if i == 145:
+                p.setJointMotorControl2(robot_id2, 2, p.POSITION_CONTROL, targetPosition=0.8, force=4500)
+                p.setJointMotorControl2(robot_id2, 3, p.POSITION_CONTROL, targetPosition=-0.8, force=4500)
+            elif i == 150:
+                p.setJointMotorControl2(robot_id2, 2, p.POSITION_CONTROL, targetPosition=-0.8, force=4500)
+                p.setJointMotorControl2(robot_id2, 3, p.POSITION_CONTROL, targetPosition=0.8, force=4500)
+            elif i == 165:
+                p.setJointMotorControl2(robot_id2, 2, p.POSITION_CONTROL, targetPosition=0, force=4500)
+                p.setJointMotorControl2(robot_id2, 3, p.POSITION_CONTROL, targetPosition=0, force=4500)
+
         if i in [125, 275]:
             contacts = p.getContactPoints(bodyA=robot_id if i == 125 else robot_id2, bodyB=ball_id)
             if contacts:
@@ -69,8 +80,9 @@ if __name__ == "__main__":
     physics_client = p.connect(p.GUI)
     setup_environment()
 
-    robot_id = load_robot("/Users/httpsbina/ARISE2024_students/1-python/3R/fingeredu.urdf", [0, 0, 0.1])
-    robot_id2 = load_robot("/Users/httpsbina/ARISE2024_students/1-python/3R/fingeredu.urdf", [0.5, 0, 0.1], baseOrientation=[0, 0, 1, 0])
+    robot_id = load_robot("/Users/httpsbina/ARISE2024_students/1-python/3R/fingeredu.urdf", [-0.1, 0, 0.1])
+    robot_id2 = load_robot("/Users/httpsbina/ARISE2024_students/1-python/3R/fingeredu.urdf", [0.3, 0, 0.1], baseOrientation=[0, 0, 1, 0])
+
     
     ball_id, constraint_id = create_ball()
     simulate_kick(robot_id, robot_id2, ball_id, constraint_id)
